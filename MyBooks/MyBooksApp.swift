5cae36f471b83b560cf7870561d8c7a56a89fe22
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-10-29 21:11:48 +0100

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Leo A.Molina on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
