9361084b9c17a81cc08cd77d7eb00921e2826c74
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-11-03 18:13:07 +0100

    Containers, Schema and configuration - Built new Schema with personalized name for the app to store the data

--- MyBooks/MyBooksApp.swift
+++ MyBooks/MyBooksApp.swift
@@ -10,14 +10,32 @@ import SwiftData
 
 @main
 struct MyBooksApp: App {
+    let container: ModelContainer
     var body: some Scene {
         WindowGroup {
             BookListView()
         }
-        .modelContainer(for: Book.self)
+        .modelContainer(container)
     }
     
     init() {
-        print(URL.applicationSupportDirectory.path(percentEncoded: false))
+        // Uses the library folder for storing the .store
+        let schema = Schema([Book.self])
+        let config = ModelConfiguration("MyBooks",schema: schema)
+        do {
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+           fatalError("Could not configure the container!")
+        }
+        // Creating a new .store path for the DB to create a container and get the data from the context. Uses the Documents folder
+        /*
+        let config = ModelConfiguration(url: URL.documentsDirectory.appending(path: "MyBooks.store"))
+        do {
+            container = try ModelContainer(for: Book.self, configurations: config)
+        } catch {
+            fatalError("Could not configure the container!")
+        }
+        */
+        print(URL.documentsDirectory.path())
     }
 }
